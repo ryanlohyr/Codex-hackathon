dcf3fb6dd85c6fb77f628ae5dc2d8b3b40c3e7ae
AlfredBeNoel <alfredbenoel@gmail.com>
2026-02-28 16:37:32 +0800

    fix deplotment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type PluginOption } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { nitro } from 'nitro/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [tailwindcss(), tsConfigPaths(), tanstackStart(), nitro()],
+  plugins: [tailwindcss(), tsConfigPaths(), tanstackStart(), react()] as PluginOption[],
   server: {
     allowedHosts: true,
   },
 })
+
